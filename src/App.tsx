01301a6ec668dcbaaebe87deea68b7298fbdc447
Vhen Larson Dela Cuesta <vhenlarsondelacuesta.1398@gmail.com>
2025-01-23 15:21:30 +0800

    add layout and hex code colors images

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,19 @@ import "./App.css";
 function App() {
   return (
     <div className="w-screen h-screen flex flex-col justify-center items-center gap-y-8">
-      <img src="assets/images/sesimi-logo.svg" alt="" />
-      <p className={`text-xl`}>Welcome to sesimi!</p>
+      <img src="assets/images/exam/colors.png" alt="Sesimi colors" />
+      <div className={`flex gap-x-8`}>
+        <img
+          src="assets/images/exam/1080x1080.png"
+          alt="Social Post"
+          className={`h-88 h-96`}
+        />
+        <img
+          src="assets/images/exam/1080x1920.png"
+          alt="Social Story"
+          className={`h-88 h-96`}
+        />
+      </div>
     </div>
   );
 }
